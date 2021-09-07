0c348c50ae9a9ce6f8a66a7cc8b76f33ba8b41ab
Fu <yangyang.fu@tamu.edu>
2021-09-07 14:27:37 -0500

    udpate modelica source

--- dSpaceExample/FiveZoneG36/sources/extendedIncludes.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTENDEDINCLUDE_H 
-#define EXTENDEDINCLUDE_H 
-#include "dsmodelext1.c" 
-#include "dsmodelext2.c" 
-#include "dsmodelext3.c" 
-#include "dsmodelext4.c" 
-#include "dsmodelext5.c" 
-#include "dsmodelext6.c" 
-#endif /* EXTENDEDINCLUDE_H */ 
